781c3c33d9462344b1ce2b61962e255f2268da51
Andrei Silva <andreisilva.n@gmail.com>
2022-09-16 14:45:04 -0300

    Added env file

---
+++ env.js
@@ -0,0 +1,7 @@
+export const env = {
+  baseUrl: "http://localhost:3000/api/v1/todos",
+  apiToken: "d9TJjEJ8DfTd2wphezrXP1VPSYZPpEem4y1u",
+  contentType: "application/json",
+  vus: 2,
+  duration: "5s",
+};
